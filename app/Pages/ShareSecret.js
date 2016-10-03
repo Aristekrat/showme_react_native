244a4962be775950ea8566d76af7c0250242fc1f
Brian Case <bcase@crossview.com>
2016-10-02 21:49:39 -0600

    Handling for the user denying contacts permissions

--- app/Pages/ShareSecret.js
+++ app/Pages/ShareSecret.js
@@ -28,8 +28,9 @@ class ShareSecret extends React.Component {
     super(props);
     this.state = {
       ph: '',
-      key: this.props.route.cookieData.key,
+      key: '',
       animating: false,
+      error: '',
     };
     uid: '';
     this.privateSecrets = this.props.db.child('privateSecrets');
@@ -38,6 +39,14 @@ class ShareSecret extends React.Component {
   }
 
   componentWillMount() {
+    if (!this.props.route.cookieData) {
+      this.setState({key: null});
+    } else if (!this.props.route.cookieData.key) {
+      this.setState({key: null});
+    } else {
+      this.setState({key: this.props.route.cookieData.key});
+    };
+
     AsyncStorage.getItem('userData')
       .then((user_data_json) => {
         if (!user_data_json) {
@@ -58,23 +67,53 @@ class ShareSecret extends React.Component {
     });
   }
 
+  // this.props.route.contacts === "PermissionDenied"
   render() {
     return (
       <View style={StylingGlobals.container}>
         <ScrollView>
-          <Text style={styles.prompt}>Choose who you want to send to...</Text>
-          <UserContacts ref="userContacts" contacts={this.props.route.contacts}/>
-          <Text style={styles.label}>You'll have a chance to review before you send</Text>
-          <BigButton do={() => 
-            { 
-              this.setState({animating: true});
-              SendSecret.saveArgs(this.refs.userContacts.state.ph, this.refs.userContacts.state.firstName, this.uid, this.state.key, this.props); 
-            }
-          }>
-            Continue
-          </BigButton>
+          {true ?
+            <View>
+              <Text style={styles.prompt}>Enter Phone Number</Text>
+              <Text style={styles.label}>Enter the phone number of who you want to send this secret to</Text>
+              <TextInput style={styles.textInput} 
+                autoFocus={true}
+                onChangeText={(ph) => this.setState({ph, error: ""})}/>
+              <Text style={styles.label}>If you let ShowMe access contacts we can find it for you</Text>
+              <BigButton do={() => { 
+                  var filteredString = this.state.ph.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(' ').join('');
+                  if (isNaN(parseInt(filteredString))) {
+                    this.setState({error: "Please enter a number"});
+                  } else if (filteredString.length < 9) {
+                    this.setState({error: "Please enter a 10 digit number"});
+                  } else {
+                    this.setState({animating: true});
+                    SendSecret.saveArgs(filteredString, "Anonymous", this.uid, this.state.key, this.props);
+                  }
+                }}>
+                Continue
+              </BigButton>
+            </View>
+            : 
+            <View>
+              <Text style={styles.prompt}>Choose who you want to send to...</Text>
+              <UserContacts ref="userContacts" contacts={this.props.route.contacts}/>
+              <Text style={styles.label}>You'll have a chance to review before you send</Text>
+              <BigButton do={() => { 
+                  this.setState({animating: true});
+                  SendSecret.saveArgs(this.refs.userContacts.state.ph, this.refs.userContacts.state.firstName, this.uid, this.state.key, this.props); 
+                }}>
+                Continue
+              </BigButton>
+            </View>
+          }
           <Text style={styles.exclusive}>Show Me is exclusively available on iPhones</Text>
           <ActivityIndicator animationControl={this.state.animating} />
+          {
+            this.state.error == "" ? null :
+            <Text style={styles.error}>{this.state.error}</Text>
+          }
+          
         </ScrollView>
         <TabBar navigator={this.props.navigator} route={this.props.route} />
       </View>
@@ -86,6 +125,8 @@ var styles = StyleSheet.create({
   prompt: {
     marginLeft: 30,
     marginTop: 20,
+    fontSize: 14,
+    fontWeight: 'bold',
   },
   textInput: {
     height: 50,
@@ -105,6 +146,12 @@ var styles = StyleSheet.create({
   exclusive: {
     marginLeft: 30,
     fontSize: 12,
+  },
+  error: {
+    marginLeft: 30,
+    color: StylingGlobals.colors.pressDown,
+    marginTop: 12,
+    fontWeight: 'bold',
   },
 });
 
--- index.ios.js
+++ index.ios.js
@@ -369,7 +369,7 @@ class ShowMe extends React.Component {
               style={styles.navBar} />
         }
         initialRoute={{
-          name: "SelectCategory"
+          name: "ShareSecret"
         }} />
     );
   }
